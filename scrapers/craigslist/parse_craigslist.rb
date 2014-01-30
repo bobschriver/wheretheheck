038c82c1509f79f51c1c634a333621db8ded230e
Bob Schriver <bobschriver@gmail.com>
2014-01-30 18:30:48 -0500

    Added db functionality. Should be fully working

--- scrapers/craigslist/parse_craigslist.rb
+++ scrapers/craigslist/parse_craigslist.rb
@@ -4,18 +4,22 @@ require 'open-uri'
 
 apartments_url = "http://seattle.craigslist.org/see/apa/"
 
+db = SQLite3::Database.new( "../../data/craigslist.db" )
+
 apartment_doc = Nokogiri::HTML(open("#{apartments_url}"))
 
 listings = apartment_doc.css("p.row")
 
+sql = ""
+
 for listing in listings
 		
-	pid = listing['data-pid']
+	pid = listing['data-pid'].to_i
 
-	latitude = listing['data-latitude']
-	longitude = listing['data-longitude']
+	latitude = listing['data-latitude'].to_f
+	longitude = listing['data-longitude'].to_f
 
-	if not latitude.nil? and not longitude.nil? 
+	if not latitude.eql? 0.0 and not longitude.eql? 0.0
 		url = listing.at_css("a.i")['href']
 		price_element = listing.at_css("span.price")	
 		size_element = listing.at_css("span.l2")
@@ -32,7 +36,9 @@ for listing in listings
 
 			price_string = price_element.children[0].text
 			size_string = size_element.children[2].text
-			
+
+			price = price_string.delete('$').to_i	
+
 			bedrooms_match = /(\d)br/.match(size_string)
 			sqft_match = /(\d+)ft/.match(size_string)
 	
@@ -42,12 +48,22 @@ for listing in listings
 				#Ruby 1.8.7 regex blows
 				bedrooms = bedrooms_match[1].to_i
 				
-				if not sqft_match.nil?
-					sqft = sqft_match[1].to_i
-				end
-
-				p "Lat #{latitude} Long #{longitude} Price #{price_string} BR #{bedrooms} SQFT #{sqft} Neighborhood #{neighborhood_string}"
+			else
+				bedrooms = 0
 			end
+			
+			#Ignore is for matching pid's
+			if not sqft_match.nil?
+				sqft = sqft_match[1].to_i
+			else	
+				sqft = 0
+			end
+
+			sql += "insert or ignore into apartments (pid, url, longitude, latitude, price, bedrooms, square_feet, neighborhood_string) values (#{pid}, '#{url}', #{longitude}, #{latitude}, #{price}, #{bedrooms}, #{sqft}, '#{neighborhood_string}');"
 		end
 	end
 end
+
+p sql
+
+db.execute_batch(sql)
