92c1782c79f8b1b2bce46eb2b4f3176e5432fb44
Bob Schriver <bobschriver@gmail.com>
2014-02-09 21:04:33 -0500

    Added categories from database for yelp

--- scrapers/yelp/yelp_scraper.rb
+++ scrapers/yelp/yelp_scraper.rb
@@ -12,7 +12,11 @@ max_results = 800
 api_host = "api.yelp.com"
 search_path = "/v2/search?"
 
-categories = ["grocery" , "bars" , "restaurants" , "food"]
+$yelp_db = SQLite3::Database.new("../../data/yelp.db")
+
+categories = $yelp_db.execute("select category_id, category_name, num_results from categories")
+categories.shuffle!
+
 
 #Need to convert from metro neighborhoods to yelp neighborhoods. Only a few need to be modified
 #So we just do it here rather than having a seperate table. todo
@@ -31,12 +35,10 @@ neighborhoods = neighborhoods_db.execute("select (neighborhood_name) from neighb
 		neighborhood_name[0].gsub(" ", "+")
 	end
 end
-
 neighborhoods.shuffle!
 
-$yelp_db = SQLite3::Database.new("../../data/yelp.db")
 
-def insert_businesses(access_token , base_path , max_results , base_offset, category, business_filter)
+def insert_businesses(access_token , base_path , max_results , base_offset, category, category_id, business_filter)
 	
 	#Step through 20 (max yelp allows) results at a time
 	for offset in (base_offset..max_results).step(20)
@@ -46,7 +48,8 @@ def insert_businesses(access_token , base_path , max_results , base_offset, cate
 		business_json_raw = access_token.get(path).body
 		business_json = JSON.parse(business_json_raw)
 
-		sql = ""
+		business_sql = ""
+		category_sql = ""
 
 		for business in business_json['businesses']
 
@@ -62,11 +65,13 @@ def insert_businesses(access_token , base_path , max_results , base_offset, cate
 				url = business['url']
 			
 				#Could do two seperate inserts here rather than passing the category as a parameter
-				sql += "insert or ignore into business (yelp_id, category, rating, num_ratings, location, url) values ('#{id}', '#{category}', #{rating}, #{review_count}, '#{location.first.gsub("'", "")}', '#{url}');"
+				business_sql += "insert or ignore into businesses (yelp_id, rating, num_ratings, location, url) values ('#{id}', #{rating}, #{review_count}, '#{location.first.gsub("'", "")}', '#{url}');"
+				category_sql += "insert into business_category (yelp_id, category_id) values ('#{id}', #{category_id});"
 			end
 		end
 		
-		$yelp_db.execute_batch(sql)
+		$yelp_db.execute_batch(business_sql)
+		$yelp_db.execute_batch(category_sql)
 		
 		p "Inserted #{base_path}"
 	end
@@ -77,10 +82,16 @@ access_token = OAuth::AccessToken.new(consumer, $token, $token_secret)
 
 for category in categories
 	for neighborhood in neighborhoods
+		
+		category_id = category[0]
+		category_name = category[1]
+		num_results = category[2]
 
-		base_path = "#{search_path}category_filter=#{category}&location=#{neighborhood}+Seattle"
+		p "#{category_name} #{neighborhood}"
+
+		base_path = "#{search_path}category_filter=#{category_name}&location=#{neighborhood}+Seattle"
 		#We go up to 39 so we just get two pages of results
-		insert_businesses(access_token , base_path, 39, 0, category, "")
+		insert_businesses(access_token , base_path, num_results, 0, category_name, category_id, "")
 		#Gotta stay under that daily request cap
 		sleep(30)
 	end
