4cf2b8e18da968341f4fe7718084b132ab2d31a5
Bob Schriver <bobschriver@gmail.com>
2014-02-14 16:42:12 -0500

    Create histogram for general transit matrix. Still a little slow

--- presentation/gen_image.py
+++ presentation/gen_image.py
@@ -1,18 +1,16 @@
 import sqlite3
 import numpy
 import scipy
+import matplotlib.pyplot
+import matplotlib.cm
 
+from matplotlib.cm import jet
+from matplotlib.pyplot import hexbin,show
 from numpy import ones,zeros,convolve,median,dstack
 from scipy.misc import imsave,imread,imresize
 from scipy.ndimage.filters import *
 
-def normalize_image(image):
-	norm_image = image * (255 / (median(image[image > 0])))
-	norm_image[norm_image > 255] = 255
-
-	return norm_image
-
-def generate_matrix(boundaries, sig_digits, data_list):
+def modify_lat_long_to_x_y(boundaries, sig_digits, data_list):
 	north_bound = boundaries[0]
 	south_bound = boundaries[1]
 	east_bound = boundaries[2]
@@ -23,30 +21,39 @@ def generate_matrix(boundaries, sig_digits, data_list):
 	height = int((north_bound - south_bound) * sig_digits_shift) 
 	width = int(abs(east_bound - west_bound) * sig_digits_shift)
 
-	matrix = zeros((height, width))
+	shifted_data = [[] , [], []]	
+
+	median_value = median([data[2] for data in data_list])
+	norm_median = 128
+	norm_max = 255
 
 	for data in data_list:
 		latitude = data[0]
 		longitude = data[1]
 		value = data[2]
-	
-		if (north_bound > latitude > south_bound) and (west_bound > longitude > east_bound):
-			x_index = int(abs(longitude - east_bound) * sig_digits_shift)
-			y_index = int((north_bound - latitude) * sig_digits_shift)
+		norm_value = value * (norm_median / median_value)
+		
+		if norm_value > norm_max:
+			norm_value = norm_max
 
-			matrix[y_index, x_index] += value
-	
-	return matrix
+		x_index = int(abs(longitude - east_bound) * sig_digits_shift)
+		y_index = int((latitude - south_bound) * sig_digits_shift)
 
-def generate_general_transit_matrix(boundaries, sig_digits):
+		shifted_data[0].append(x_index)
+		shifted_data[1].append(y_index)
+		shifted_data[2].append(norm_value)
+	
+	return shifted_data
+
+def generate_general_transit_list(boundaries, sig_digits):
 	conn = sqlite3.connect('../data/busses.db')
 	cursor = conn.cursor()
 
-	stops = cursor.execute("select latitude,longitude,trips_count from stops")
+	stops = cursor.execute("select latitude,longitude,trips_count from stops").fetchall()
 
-	gtm = generate_matrix(boundaries, sig_digits, stops)
+	gtl = modify_lat_long_to_x_y(boundaries, sig_digits, stops)
 
-	return gtm
+	return gtl
 
 def generate_neighborhood_destination_transit_matrix(boundaries, sig_digits, neighborhoods):
 	conn = sqlite3.connect('../data/busses.db')
@@ -104,13 +111,9 @@ def generate_apartment_cost_matrix(boundaries, sig_digits):
 	#Aparently the iterator is consumed, so we just fetch all results here
 	cost_raw = cursor.fetchall()
 	
-	prices_per_sqft = [cost_data[2] / float(cost_data[3]) for cost_data in cost_raw]
-	prices_per_sqft_median = median(prices_per_sqft)
-
 	#This will create a negative value for apartments above the price / sqft median
 	#and positive for those above it
-	cost = [[cost_data[0], cost_data[1], (prices_per_sqft_median - (cost_data[2] / float(cost_data[3]))) * 50] for cost_data in cost_raw]
-	print cost
+	cost = [[cost_data[0], cost_data[1], (cost_data[2] / float(cost_data[3])) * 50] for cost_data in cost_raw]
 
 	acm = generate_matrix(boundaries, sig_digits, cost)
 
@@ -126,26 +129,22 @@ boundaries = [north_bound, south_bound, east_bound, west_bound]
 
 sig_digits = 4
 
+
 print "Creating apartment cost matrix"
-acm = generate_apartment_cost_matrix(boundaries, sig_digits)
-imsave("acm.tiff", gaussian_filter(acm , 10))
+#acl = generate_apartment_cost_list(boundaries, sig_digits)
 
-print "Creating general transit matrix"
-gtm = generate_general_transit_matrix(boundaries, sig_digits)
-gtm_norm = normalize_image(gtm)
-imsave("gtm_norm.tiff", gaussian_filter(gtm_norm, 10))
+print "Creating general transit list"
+gtl = generate_general_transit_list(boundaries, sig_digits)
+print len(gtl[0]),len(gtl[1]),len(gtl[2])
+hexbin(gtl[0], gtl[1], gtl[2], gridsize=(40,60), cmap=jet)
+show()
 
-print "Creating neighborhood destination transit matrix"
+print "Creating neighborhood destination transit list"
 neighborhoods = ["South Lake Union", "Delridge", "Capitol Hill", "Fremont"]
-ndtm = generate_neighborhood_destination_transit_matrix(boundaries, sig_digits, neighborhoods)
-imsave("ndtm_norm.tiff", gaussian_filter(ndtm, 10))
+#ndtl = generate_neighborhood_destination_transit_list(boundaries, neighborhoods)
 
 print "Creating business quality matrix"
 categories = [['markets', 20] , ['grocery' , 15] , ['restaurants' , 10] , ['bars' , 5]]
-bqm = generate_business_quality_matrix(boundaries, sig_digits, categories)
-bqm_norm = normalize_image(bqm)
-imsave("bqm_norm.tiff", gaussian_filter(bqm_norm , 10))
+#bql = generate_business_quality_list(boundaries, categories)
 
-print "Creating final image"
-total_norm = gtm_norm + ndtm + bqm_norm + acm
-imsave("total.tiff", gaussian_filter(total_norm, 10))
+
